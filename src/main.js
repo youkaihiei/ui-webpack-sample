5890b284775c1baf8ced8994333f62f85f4c2e1e
Nathaniel Thomas <hello@nathanielthomas.net>
2017-03-30 17:56:18 -0700

    Add babelrc file

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import Form from './js/form';
 import Global from './js/global';
 import Home from './js/home';
 import Sidebar from './js/sidebar';
-let path = require('path');
 require('bootstrap-sass');
 require('./css/style.scss');
 
